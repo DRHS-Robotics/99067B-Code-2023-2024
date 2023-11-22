5c743ad874fc2124c62ca4020c4a22bf10e0b72d
A5bhinav <abhinavjinka@gmail.com>
2023-11-22 09:37:30 -0700

    adding files

---
+++ DR-Skills/include/classDefinitions/tapa.h
@@ -0,0 +1,13 @@
+#ifndef TAPA_h
+#define TAPA_h
+
+class tapaSpeed{
+	public:
+		int tapaMatchLoad;
+		int tapaSingleShot;
+        void tapaSet(int tapaMatch, int tapaSingle);
+		
+};
+
+
+#endif
---
+++ DR-Skills/include/variables.h
@@ -0,0 +1,31 @@
+#ifndef VARIABLES_h
+#define VARIABLES_h
+
+extern bool frontSlapaState;
+extern bool backSlapaState;
+extern const double maxtapaShoot;
+extern DrivePID Drive;
+extern tapaSpeed tapaSpeedControl;
+extern ArcTurn arcTurn;
+
+extern double arcIntegral;
+extern double arcDerivative;
+extern double arcLastError;
+extern double driveProportion;
+extern double driveDerivative;
+extern double driveLastError;
+extern double angleProportion;
+extern double angleDerivative;
+extern double angleLastError;
+
+extern double turnIntegral; 
+extern double turnDerivative;
+extern double turnLastError;
+
+extern pros::Task* tapaTask;
+extern pros::Task* autoTapaTask;
+
+extern bool moveState;
+
+
+#endif
