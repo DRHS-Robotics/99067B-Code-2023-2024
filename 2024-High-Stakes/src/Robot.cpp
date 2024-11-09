2e523674e46272d23acc80f8f4b7c267eb3e2f08
A5bhinav <abhinavjinka@gmail.com>
2024-11-09 00:20:54 -0700

    final

--- 2024-High-Stakes/src/Robot.cpp
+++ 2024-High-Stakes/src/Robot.cpp
@@ -15,7 +15,7 @@ class Robot{
         // Rotation horizontalRot;
 
         Robot(std::initializer_list<std::int8_t> leftMotors, std::initializer_list<std::int8_t> rightMotors, int intake, int conveyor, int imu1,  MotorGears greenGear, MotorGears blueGear) 
-        : leftMotors(leftMotors, blueGear), rightMotors(rightMotors, blueGear), intake(intake, greenGear), conveyor(conveyor), imu1(imu1) {
+        : leftMotors(leftMotors, blueGear), rightMotors(rightMotors, blueGear), intake(intake, greenGear), conveyor(conveyor, blueGear), imu1(imu1) {
             // this->verticalRot.set_reversed(true);
             // this->horizontalRot.set_reversed(false);
         }
